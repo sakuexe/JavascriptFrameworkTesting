75e0f6aa1aa2553313b2a91973b690aac012094b
sakuexe <saku.karttunen@student.hamk.fi>
2022-10-02 13:04:58 +0300

    changed tailwindcss file location

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './vanilla/*{html,js}',
+    './react/*{html,js}',
+    './vue/*{html,js}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
